54d0f6bd41a75019ab55ecf514cbe1bcefd14a56
Asvolk0 <pennequincarl@gmail.com>
2022-04-19 00:38:24 +0200

    ajout des messages envoyé aux admins

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,18 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Message;
+use App\Entity\User;
+use App\Form\FormMessageType;
+use App\Repository\MessageRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message', methods:['GET', 'POST'])]
-    public function message()
+    public function message(Request $request, MessageRepository $messageRepository, UserRepository $userRepository)
     {
         $message = new Message();
+        $formMessage = $this->createForm(FormMessageType::class, $message);
+        $formMessage->handleRequest($request);
 
+        if ($formMessage->isSubmitted() && $formMessage->isValid()){
+            $message->setUser($this->getUser());
+            $messageRepository->add($message);
+
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        
+        
 
         return $this->render('message/message.html.twig', [
+            'formMessage'=>$formMessage->createView(),
+            "message"=>$message
         ]);
     }
 }
--- src/Form/FormMessageType.php
+++ src/Form/FormMessageType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Message;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +19,28 @@ class FormMessageType extends AbstractType
             ->add('name')
             ->add('lastname')
             ->add('email')
-            ->add('message')
+            ->add('message', TextareaType::class, [
+                'attr'=> array(
+                    'style'=> 'width: 400px; 
+                    max-width: 400px; 
+                    min-width: 400px; 
+                    height: 40px; 
+                    min-height: 40px; 
+                    max-height: 3000px; 
+                    font-size: 25px; 
+                    padding: 3px;
+                    border: none;
+                    border-bottom: 1px solid #707070;
+                    margin-top: 6px;
+                    ',
+                )
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=>'>',
+                'attr' => array(
+                    'class' => 'btn'
+                )
+            ])
         ;
     }
 
