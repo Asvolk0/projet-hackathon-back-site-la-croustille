cb953189e87de1902772a1d3d8eca25b73f59f59
Asvolk0 <pennequincarl@gmail.com>
2022-04-25 12:14:51 +0200

    form add recipe = ok, traitement img = en cours

--- src/Controller/AddRecipeController.php
+++ src/Controller/AddRecipeController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
+use App\Entity\Step;
+use App\Entity\Type;
 use App\Repository\RecipeRepository;
 use App\Repository\StepRepository;
+use App\Repository\TypeRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,42 +17,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class AddRecipeController extends AbstractController
 {
     #[Route('/ajouterUneRecette', name: 'app_add_recipe')]
-    public function app_add_recipe(Request $request, RecipeRepository $recipeRepository, StepRepository $stepRepository)
+    public function app_add_recipe(Request $request, RecipeRepository $recipeRepository, StepRepository $stepRepository, TypeRepository $typeRepository, ManagerRegistry $doctrine)
     {
-        $recipe = new Recipe();
+        $entityManager = $doctrine->getManager();
 
-        
-        if ($request->request->get('title') && $request->request->get('img') &&
-        $request->request->get('type') && $request->request->get('stepFirst')){
-            $step['stepFirst'] = $request->request->get('stepFirst');
-            $step['stepSecond'] = $request->request->get('stepSecond');
-            $step['stepThree'] = $request->request->get('stepThree');
-            $step['stepFour'] = $request->request->get('stepFour');
+        $recipe = new Recipe();
+        $type = new Type();
+
+        $types = $typeRepository->findBy([], ['name' => 'DESC']);
+
+        if (!empty($request->request->all()))
+        {
+            $stepForm['stepFirst'] = $request->request->get('stepFirst');
+            $stepForm['stepSecond'] = $request->request->get('stepSecond');
+            $stepForm['stepThree'] = $request->request->get('stepThree');
+            $stepForm['stepFour'] = $request->request->get('stepFour');
 
             $recipes['title'] = $request->request->get('title');
-            $recipes['img'] = $request->request->get('img');
+            $img = $request->request->get('img');
+            $imgName = md5(uniqid()) . '.' .$img->guessExtension();
+            $img->move($this->getParameter('uploadDirectory'), $imgName);
 
-            $type = $request->request->get('type');
-
-            dd($type, $recipe->setTitle($recipes['title'])
-                            ->setImg($recipes['img'])
-                            // ->addStep($step)
-                            ->setActive(true));
-
+            $typeTake = $request->request->get('type');
 
             $recipe->setTitle($recipes['title'])
-                   ->setImg($recipes['img'])
-                   ->setType($recipes['type'])
+                   ->setImg($imgName)
                    ->setActive(true);
 
+            $type->setName($typeTake);
 
-            $recipeRepository->add($recipe);
+            $type->addRecipe($recipe);
 
-            
-            
-            dd($recipes, $step, $recipe);
-        }  
+            foreach ($stepForm as $step){
+                $newStep = new Step();
 
-        return $this->render('recipe/addRecipe.html.twig');
+                $newStep->setSteps($step);
+
+                $recipe->addStep($newStep);
+                $entityManager->persist($newStep);
+            }
+
+
+            $entityManager->persist($type);
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+        }
+
+        return $this->render('recipe/addRecipe.html.twig', [
+            'types' => $types
+        ]);
     }
 }
